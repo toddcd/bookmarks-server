66425db5d7f82794b8fbc3df9ae3cbef4f776649
Todd Davis <tejastcd@gmail.com>
2019-07-18 10:08:16 -0400

    updated database url for use on heroku

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-  DB_URL: process.env.DB_URL || 'postgresql://bookmark-admin:thinkful19@localhost/bookmarks',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgres://fclxyokgxebguo:72d25fc27775f5a55eb26c9da579df0b3f3ba50b81455abea2acfc632622ba44@ec2-54-235-246-201.compute-1.amazonaws.com:5432/d53uglcjmgv6rd',
 }
